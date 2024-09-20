d4db440716dcd23a8a6ade27f70cb13696eff974
pembattech <pembat.tech@gmail.com>
2024-09-20 11:42:16 +0545

    Update template and routes.

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::resource('students', StudentController::class);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::redirect('/', '/students')->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('students', StudentController::class);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
